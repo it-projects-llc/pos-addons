4081e0fb7a3e399da1951e50ca30216e939c9946
Eugene Molotov <molotov@it-projects.info>
2023-10-11 13:28:19 +0500

    [FIX] pos_esign_request: убираю ненужный метод, чтобы не вмешивался в работу модуля pos_longpolling

--- pos_esign_request/static/src/js/esign_mode.js
+++ pos_esign_request/static/src/js/esign_mode.js
@@ -255,27 +255,9 @@ var KioskMode = AbstractAction.extend({
 
 core.action_registry.add('est_kiosk_mode', KioskMode);
 
-// TODO: remove the block below if odoo fixed the issue described here https://github.com/odoo/odoo/pull/28092
-var CrossTab = require('bus.CrossTab');
-CrossTab.include({
-    _heartbeat: function() {
-        this._super();
-        // https://github.com/odoo/odoo/pull/28092
-        var hbPeriod = this.TAB_HEARTBEAT_PERIOD;
-        if (this._isMasterTab) {
-            hbPeriod = this.MASTER_TAB_HEARTBEAT_PERIOD;
-        }
-        if (this._heartbeatTimeout) {
-            clearTimeout(this._heartbeatTimeout);
-        }
-        this._heartbeatTimeout = setTimeout(this._heartbeat.bind(this), hbPeriod);
-    }
-});
-
 return {
     KioskMode: KioskMode,
     AcceptModalKiosk: AcceptModalKiosk,
-    CrossTab: CrossTab,
 };
 
 });
