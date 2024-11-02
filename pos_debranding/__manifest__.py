bf2fccbd30ee19f40b582a16bf5130ed6a448bae
Eugene Molotov <molotov@it-projects.info>
2024-11-02 14:56:33 +0500

    [ADD] pos_debranding

---
+++ pos_debranding/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "POS debranding",
+    "version": "17.0.1.0.0",
+    "author": "IT-Projects LLC",
+    "license": "LGPL-3",
+    "category": "Debranding",
+    "website": "https://github.com/it-projects-llc/pos-addons",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        "views.xml",
+    ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_debranding/static/src/xml/pos_debranding.xml",
+        ]
+    },
+}
