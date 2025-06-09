17f86ab322d1dfdc63966bc659ea91892d901246
Eugene Molotov <molotov@it-projects.info>
2025-06-09 10:01:16 +0500

    [FIX] pos_partner_deselection: перезапускаем таймер при смене экрана
    Также учитываем следующие ситуации перед запуском таймера снятия клиента
    
    1. Если выставлен to invoice, то не запускаем таймер
    2. В экране списка заказов не запускаем таймер

--- pos_partner_deselection/static/src/js/partner_deselection.js
+++ pos_partner_deselection/static/src/js/partner_deselection.js
@@ -2,38 +2,58 @@ odoo.define("pos_partner_deselection.partner_deselection", [], function (require
     "use strict";
 
     const {Order} = require("@point_of_sale/app/store/models");
+    const {PosStore} = require("@point_of_sale/app/store/pos_store");
     const {patch} = require("@web/core/utils/patch");
+
+    patch(PosStore.prototype, {
+        showScreen() {
+            super.showScreen(...arguments);
+            this.setPartnerDeselectTimer();
+        },
+
+        setPartnerDeselectTimer() {
+            clearTimeout(this.partnerDeselectTimer);
+            if (this.shouldResetPartnerDeselectTimer(this.selectedOrder)) {
+                this.partnerDeselectTimer = setTimeout(
+                    () => this.deselectPartner(),
+                    this.config.customer_deselection_interval * 1000
+                );
+            }
+        },
+
+        shouldResetPartnerDeselectTimer(order) {
+            return (
+                this.mainScreen?.component?.name !== "TicketScreen" &&
+                this.config.customer_deselection_interval &&
+                order &&
+                order.partner &&
+                !order.to_invoice
+            );
+        },
+
+        deselectPartner() {
+            if (!this.selectedOrder.finalized && !this.selectedOrder.to_invoice) {
+                this.selectedOrder.set_partner(null);
+            }
+        },
+    });
 
     patch(Order.prototype, {
         setup() {
             super.setup(...arguments);
-            if (this.partner) {
+            if (this.pos.shouldResetPartnerDeselectTimer(this)) {
                 this.set_partner(null);
             }
         },
 
-        setupCustomerDeselection(interval) {
-            if (this.partner_deselect_timer) {
-                clearTimeout(this.partner_deselect_timer);
-            }
-            this.partner_deselect_timer = setTimeout(() => {
-                if (!this.finalized) {
-                    this.set_partner(null);
-                }
-            }, interval * 1000);
+        set_partner() {
+            super.set_partner(...arguments);
+            this.pos.setPartnerDeselectTimer();
         },
 
-        set_partner(partner) {
-            super.set_partner(...arguments);
-
-            const customer_deselection_interval =
-                this.pos.config.customer_deselection_interval;
-
-            if (customer_deselection_interval && partner && !this.finalized) {
-                this.setupCustomerDeselection(customer_deselection_interval);
-            } else if (!partner && this.partner_deselect_timer) {
-                clearTimeout(this.partner_deselect_timer);
-            }
+        set_to_invoice() {
+            super.set_to_invoice(...arguments);
+            this.pos.setPartnerDeselectTimer();
         },
     });
 });
