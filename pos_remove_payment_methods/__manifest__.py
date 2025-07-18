22781098cbd1692280fee99b3116ba6bf26dc66e
kopeyev <kopeyev07@gmail.com>
2025-07-19 01:59:40 +0500

    [IMP] pre-commit's improvements

--- pos_remove_payment_methods/__manifest__.py
+++ pos_remove_payment_methods/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": "Clears payment lines when re-entering the POS payment screen",
     "author": "Almas Kopeyev, IT-Projects LLC",
     "version": "16.0.1.0.0",
-    "website": "https://github.com/it-projects-llc",
+    "website": "https://github.com/it-projects-llc/pos-addons",
     "category": "Point of Sale",
     "depends": ["point_of_sale"],
     "maintainers": [],
