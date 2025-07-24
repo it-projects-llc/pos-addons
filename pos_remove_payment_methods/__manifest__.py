25c4c21bb43ee2adc792514b483c66c7076a8d5e
kopeyev <kopeyev07@gmail.com>
2025-07-25 01:39:42 +0500

    [IMP] Moved remove_paymentlines from onCustomBackClick to showScreen, removed unused xml

--- pos_remove_payment_methods/__manifest__.py
+++ pos_remove_payment_methods/__manifest__.py
@@ -3,18 +3,13 @@
     "summary": "Clears payment lines when re-entering the POS payment screen",
     "author": "Almas Kopeyev, IT-Projects LLC",
     "version": "16.0.1.0.0",
-    "website": "https://github.com/it-projects-llc/pos-addons",
-    "category": "Point of Sale",
+    "website": "https://github.com/it-projects-llc",
     "depends": ["point_of_sale"],
-    "maintainers": [],
     "data": [],
     "assets": {
         "point_of_sale.assets": [
             "pos_remove_payment_methods/static/src/**/*",
         ]
     },
-    "license": "AGPL-3",
-    "installable": True,
-    "auto_install": False,
-    "application": False,
+    "license": "LGPL-3",
 }
--- pos_remove_payment_methods/static/src/js/PaymentScreen.js
+++ pos_remove_payment_methods/static/src/js/PaymentScreen.js
@@ -1,24 +1,26 @@
-odoo.define("pos_remove_payment_methods.PaymentScreen", function (require) {
-    "use strict";
+odoo.define('pos_remove_payment_methods.PaymentScreen', function (require) {
+    'use strict';
 
-    const PaymentScreen = require("point_of_sale.PaymentScreen");
-    const Registries = require("point_of_sale.Registries");
-    const NumberBuffer = require("point_of_sale.NumberBuffer");
+    const PaymentScreen = require('point_of_sale.PaymentScreen');
+    const Registries = require('point_of_sale.Registries');
+    const NumberBuffer = require('point_of_sale.NumberBuffer');
 
     const PosRemovePaymentScreen = (PaymentScreen) =>
         class extends PaymentScreen {
-            onCustomBackClick() {
-                const currentOrder = this.env.pos.get_order();
-                if (currentOrder) {
-                    const lines = [...currentOrder.get_paymentlines()];
-                    lines.forEach((line) => currentOrder.remove_paymentline(line));
+            showScreen(screenName, ...args) {
+                const result = super.showScreen(screenName, ...args);
+                if (screenName === 'ProductScreen' && this.currentOrder) {
+                    [...this.currentOrder.get_paymentlines()].forEach(line => {
+                        this.currentOrder.remove_paymentline(line);
+                    });
                     NumberBuffer.reset();
+                    this.render(true);
                 }
-                this.showScreen("ProductScreen");
+                return result;
             }
         };
 
     Registries.Component.extend(PaymentScreen, PosRemovePaymentScreen);
 
-    return PosRemovePaymentScreen;
+    return PaymentScreen;
 });
