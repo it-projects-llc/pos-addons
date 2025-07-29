d6abdecc3a1036d2f343ff00f29b1cf6a301fc8a
kopeyev <kopeyev07@gmail.com>
2025-07-29 19:35:59 +0500

    [IMP] pre-commit's fixes

--- pos_remove_payment_methods/static/src/js/PaymentScreen.js
+++ pos_remove_payment_methods/static/src/js/PaymentScreen.js
@@ -1,16 +1,16 @@
-odoo.define('pos_remove_payment_methods.PaymentScreen', function (require) {
-    'use strict';
+odoo.define("pos_remove_payment_methods.PaymentScreen", function (require) {
+    "use strict";
 
-    const PaymentScreen = require('point_of_sale.PaymentScreen');
-    const Registries = require('point_of_sale.Registries');
-    const NumberBuffer = require('point_of_sale.NumberBuffer');
+    const PaymentScreen = require("point_of_sale.PaymentScreen");
+    const Registries = require("point_of_sale.Registries");
+    const NumberBuffer = require("point_of_sale.NumberBuffer");
 
     const PosRemovePaymentScreen = (PaymentScreen) =>
         class extends PaymentScreen {
             showScreen(screenName, ...args) {
                 const result = super.showScreen(screenName, ...args);
-                if (screenName === 'ProductScreen' && this.currentOrder) {
-                    [...this.currentOrder.get_paymentlines()].forEach(line => {
+                if (screenName === "ProductScreen" && this.currentOrder) {
+                    [...this.currentOrder.get_paymentlines()].forEach((line) => {
                         this.currentOrder.remove_paymentline(line);
                     });
                     NumberBuffer.reset();
