10725e5664d9295e74fdf63d325d7ec154591cd4
Almas Kopeyev <kopeyev@users.noreply.github.com>
2025-08-04 11:16:36 +0500

    [ADD] pos_remove_payment_methods (#13)

---
+++ pos_remove_payment_methods/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "POS: clear the payment lines on screen entry",
+    "summary": (
+        "Clears the payment lines that have been selected before "
+        "in case when we return back to the cart and re-entering the POS payment screen"
+    ),
+    "author": "Almas Kopeyev, IT-Projects LLC",
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/it-projects-llc/pos-addons",
+    "depends": ["point_of_sale"],
+    "data": [],
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_remove_payment_methods/static/src/**/*",
+        ]
+    },
+    "license": "LGPL-3",
+}
