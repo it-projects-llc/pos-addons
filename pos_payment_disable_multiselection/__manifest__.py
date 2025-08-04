d0082cf181437d4b5a79ad7310b743bb38ebe1e9
Almas Kopeyev <kopeyev@users.noreply.github.com>
2025-08-04 11:13:05 +0500

    [ADD] pos_payment_disable_multiselection (#12)

---
+++ pos_payment_disable_multiselection/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "POS: disable multiple select for payment method",
+    "summary": "Prevent selecting the same payment method multiple times in POS",
+    "author": "Almas Kopeyev, IT-Projects LLC",
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/it-projects-llc/pos-addons",
+    "depends": ["point_of_sale"],
+    "data": [],
+    "assets": {
+        "point_of_sale.assets": [
+            "pos_payment_disable_multiselection/static/src/**/*",
+        ],
+    },
+    "license": "LGPL-3",
+}
